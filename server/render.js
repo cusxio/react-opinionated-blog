a72a1c247eea145c412575ccceee211dfd4f5eb8
Jonathan Chan <cusxio@gmail.com>
2016-03-29 17:21:55 +0800

    add context provider for ssr

--- server/render.js
+++ server/render.js
@@ -1,12 +1,29 @@
+import fs from 'fs';
+import path from 'path';
 import React from 'react';
 import Helmet from 'react-helmet';
 import { renderToString, renderToStaticMarkup } from 'react-dom/server';
 import { match, RouterContext } from 'react-router';
+import { Provider } from 'react-redux';
+import configureStore from '../app/redux/store';
 
 import getAssetsPaths from '../webpack/utils/assets-path';
-import Html from '../app/containers/Html';
+import Html from '../app/components/Html';
 // WORK: routes
 import routes from '../app/router/routes';
+
+import ContextProvider from '../app/containers/ContextProvider';
+import Home from '../app/components/Home';
+import Blog from '../app/components/Blog';
+import Post from '../app/components/Post';
+
+const layouts = {
+    Home,
+    Blog,
+    Post,
+};
+
+const pagesfromJson = JSON.parse(fs.readFileSync(path.join(__dirname, '..', '_tmp', 'data.json'), 'utf-8'));
 
 export default async function render(ctx, next) {
     const isHtml = ctx.headers.accept && ctx.accepts('html');
@@ -22,7 +39,13 @@ export default async function render(ctx, next) {
         } else if (redirectLocation) {
             ctx.redirect(redirectLocation.pathname + redirectLocation.search);
         } else if (renderProps) {
-            const markup = renderToString(<RouterContext {...renderProps} />);
+            const markup = renderToString(
+                <ContextProvider layouts={layouts} pages={pagesfromJson}>
+                    <Provider store={configureStore()}>
+                        <RouterContext {...renderProps} />
+                    </Provider>
+                </ContextProvider>
+                );
             const { title, meta } = Helmet.rewind();
             const head = {
                 title: title.toComponent(),
